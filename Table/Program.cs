cc3aecf8cbee7ae050086098b8f091a009461181
Alpaga65 <dorian.marco@viacesi.fr>
2018-12-06 08:38:10 +0100

    Validation du projet test

---
+++ Table/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Table
+{
+    class Table
+    {
+        
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public Table()
+        {
+            int IDTable = 0;
+            int rang = 0;
+            int carre = 0;
+            int NumberPerson = 0;
+            int IDEmployee = 0;
+
+            Boolean AvailableTable = true;            
+            Boolean Clean = true;            
+            Boolean Water = false;
+            Boolean Bread = false;
+        }
+
+        public Table(int IDTable, int rang, int carre, int NumberPerson, int IDEmployee, Boolean AvailableTable, Boolean Clean, Boolean Water, Boolean Bread) {
+
+        }
+
+
+    }
+}
---
+++ TestUnitaire/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnitaire")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnitaire")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f62ab5f7-55b9-4f8e-b2bf-7eb66f1ac809")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
