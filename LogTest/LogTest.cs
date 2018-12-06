bec7cdd66586583dca8d5a0f5f6c87f06e5d50df
Alexandre SADOUN <alexandre.sadoun@viacesi.fr>
2018-12-06 10:13:14 +0100

    MVC

---
+++ LogTest/LogTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace LogTest
+{
+    [TestClass]
+    public class LogTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            using (StreamReader reader = new StreamReader(@"C:\Users\sadoun\Desktop\projetc#\DossierLog\log.txt"))
+            {
+                string content = reader.ReadToEnd();
+                reader.Close();
+                String b = content;
+                Console.WriteLine(b);
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
