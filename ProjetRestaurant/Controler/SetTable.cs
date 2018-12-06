e70027f8c7a7e9fd659b1fce65780b5d62c8bb93
Alpaga65 <dorian.marco@viacesi.fr>
2018-12-06 17:00:48 +0100

    Code du 06/12/2018

---
+++ ProjetRestaurant/Controler/SetTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controler
+{
+    internal class SetTable
+    {
+       
+        public Boolean Set(int NumberPerson, int Table)
+        {
+            int NbrPerson = NumberPerson;
+            int IDTable = Table;
+            Boolean Available = false;       //SQL NEEDED
+            Boolean OtherTable = true;
+            if (Available == true)
+            {
+                Console.WriteLine("Bonne table");
+                Console.ReadKey();
+                return true;
+            }else
+            {
+                if (OtherTable == true)     //SQL NEEDED      
+                {
+                    Console.WriteLine("Table de taille supérieur");
+                    Console.ReadKey();
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Pas de place DESO");
+                    Console.ReadKey();
+                    return false;
+                }
+            }
+            
+        }
+    }   
+}
---
+++ ProjetRestaurant/Controler/Table.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controler
+{
+    class Table
+    {
+        
+        private static void Main(string[] args)
+        {
+            int person = 5;
+
+            CheckTable CheckTest = new CheckTable();
+            SetTable SetTest = new SetTable();
+            int Checktest = CheckTest.Check(person);
+            Boolean Settest = SetTest.Set(person, Checktest);
+
+
+            
+            
+            if (Settest == true)
+            {
+                Console.WriteLine("Vous allez avoir une table de " + Checktest);
+                Console.WriteLine("Une table est bien dispo");
+                Console.ReadKey();
+            }else if (Settest == false)
+            {
+                Console.WriteLine("DESO BRO");
+                Console.ReadKey();
+            }
+
+        }
+        public Table()
+        {
+            int IDTable = 0;
+            int Row = 0;
+            int Square = 0;
+            int NumberPerson = 4;
+            int IDEmployee = 0;
+
+            int CustomerGroup = 0;
+
+            Boolean AvailableTable = true;
+            Boolean Clean = true;
+            Boolean Water = false;
+            Boolean Bread = false;
+        }
+
+
+
+
+
+    }
+}
