fc057241d055426e73d327e5b0588882df908ec9
Alpaga65 <dorian.marco@viacesi.fr>
2018-12-09 15:18:53 +0100

    Set Table

--- ProjetRestaurant/Controler/SetTable.cs
+++ ProjetRestaurant/Controler/SetTable.cs
@@ -12,7 +12,7 @@ namespace command
 
 			
 			string requete1 = "UPDATE tables SET situation= "+ situation + " WHERE id_table=" + table;
-			string requete2 = "UPDATE clients SET id_table=" + table + " WHERE id_groupe=" + id_groupe;
+			string requete2 = "UPDATE Clients SET id_table=" + table + " WHERE id_groupe=" + id_groupe;
 			
 			MySqlCommand cmd = new MySqlCommand(requete1, connexion.connect);
 			cmd.ExecuteNonQuery();
